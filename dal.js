50f9c5f8528a69f5506162e642e28d53925b863f
Brandon Aiton <brandonaiton@icloud.com>
2017-09-06 16:46:08 -0400

    starting styling with CSS

--- dal.js
+++ dal.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/Snippets');  //I will make your DB!
-const CharacterModel = require('/characterModel')
-const UserModel = require('/userModel')
-
-function getAllCharacters(){
-  return CharacterModel.find();
-}
-
-function getAllUsers(){
-  return UserModel.find();
-}
-
-function newUser(info){
-    let newGuy = new UserModel(info)
-    newGuy.save(function(err){
-      console.log('err from newGuy', err);
-    })
-    return Promise.resolve('success');
-}
-
-module.exports = {
-  getAllUsers, getAllCharacters, newUser
-}
+// const CharacterModel = require('/characterModel')
+// const UserModel = require('/userModel')
+//
+// function getAllCharacters(){
+//   return CharacterModel.find();
+// }
+//
+// function getAllUsers(){
+//   return UserModel.find();
+// }
+//
+// function newUser(info){
+//     let newGuy = new UserModel(info)
+//     newGuy.save(function(err){
+//       console.log('err from newGuy', err);
+//     })
+//     return Promise.resolve('success');
+// }
+//
+// module.exports = {
+//   getAllUsers, getAllCharacters, newUser
+// }
