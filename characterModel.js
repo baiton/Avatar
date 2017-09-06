21b4a27b15c539244ec16bf327af077b6dd81eec
Kelly Toomes <kelly@Kellys-MacBook.local>
2017-09-06 15:00:10 -0400

    patrick's initial commit

---
+++ characterModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const CharacterSchema =  new mongoose.Schema({
+  skin_tone:{ type:String, required: true},
+  expression:{ type:String, required: true},
+  hair:{type: String, required: true}
+})
+const Character = mongoose.model('Character', CharacterSchema)
+module.export = {Character}
