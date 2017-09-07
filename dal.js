fe9546dc6489224d81c3ae3c8519cbec14ae9e8c
Patrick Toomes <pktoomes@g.cofc.edu>
2017-09-07 04:20:33 -0400

    more functions 2

--- dal.js
+++ dal.js
@@ -4,22 +4,54 @@ const User = require('./userModel.js');
 const Character = require('./characterModel.js')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/avatardb');  //I will make your DB!
-function getAllCharacters{
+function getAllCharacters(){
   return Character.find();
 }
+
+function getUserByUsername (username) {
+  return User.find({ username: username }).exec().catch(function (err) {
+    console.log('error', err)
+  })
+}
+
 function addCharacter(newCharacter){
   const character = new Character({
-    skin_tone:{ type:String, required: true},
-    expression:{ type:String, required: true},
-    hair:{type: String, required: true}
+    skin_tone: newCharacter.skin_tone,
+    expression:newCharacter.expression,
+    hair:newCharacter.hair
   })
-  console.log('Xman', Xperson);
-  Xperson.save(function(err){
+  console.log('character', character);
+  character.save(function(err){
     console.log(err)
   })
   return Promise.resolve('success')
 }
 
+function addUser(newUser){
+  const user = new User({
+    username: newUser.username,
+    password: newUser.password
+  })
+  console.log('user', user);
+  user.save(function(err){
+    console.log(err)
+  })
+  return Promise.resolve('success')
+}
+
+function updateCharacter(character){
+  Character.update({_id: character.id}, {$set:{
+    skin_tone: Character.skin_tone,
+    expression: Character.expression,
+    hair: Character.hair
+      }
+    },
+    function(err, data){
+			if (err) return handleError(err);
+			console.log("Mongo data", data)
+      }
+  )
+}
 // function getAllCharacters(){
 //   return CharacterModel.find();
 // }
