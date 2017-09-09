8fd41f85e80f4ccc53b6689c47c0f3038a2fbc85
Joshua May <jmay89@icloud.com>
2017-09-09 12:22:38 -0400

    password is not defined in app js where password is passed into a param

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const mustacheExpress = require('mustache-express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const passport = require('passport');                 //passport related
+const LocalStrategy = require('passport-local').Strategy; //passport 
+const session = require('express-session');
+const bluebird = require('bluebird');
+const bcrypt = require('bcryptjs')                 //passport related
+const flash = require('express-flash-messages');   // passport related
+
+bcrypt.compareSync(password, hash);           // passport related
+let password = newUser.psw;                   //passport related
+const hash = bcrypt.hashSync(password, 8);    //passport related
+
 const app = express();
 // const session = require('express-session')
 const dal = require('./dal.js')
@@ -13,21 +24,96 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
-//app.use(session({
-  //secret:'this is a secret',
-  //resave:false,
-  //saveUninitialized:true,
-  //cookie:{maxAge: null}
-  //})
-//)
+
+
+// session below and passport init
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());         //passport related
+app.use(passport.session());            //passport related
+app.use(flash());                       //passport related
+
 app.use(express.static('public'));
 
 
+// bcryptjs below, put on your hardhats and lets go to work people!
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, lowercase: true, required: true },
+  passwordHash: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+userSchema.virtual('password')
+  .get(function () { return null })
+  .set(function (value) {
+    const hash = bcrypt.hashSync(value, 8);
+    this.passwordHash = hash;
+  })
+
+userSchema.methods.authenticate = function (password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+
+userSchema.statics.authenticate = function(username, password, done) {
+    this.findOne({
+        username: username
+    }, function(err, user) {
+        if (err) {
+            done(err, false)
+        } else if (user && user.authenticate(password)) {
+            done(null, user)
+        } else {
+            done(null, false)
+        }
+    })
+};
+
+// passport attempt below, fasten your seatbelts people. cat5 passport
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.authenticate(username, password, function(err, user) {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
 // -------------Login--------------------
-app.get('/login', (req,res) =>{
-  res.render('./login')
+app.get('/login', function(req,res) {
+  res.render("login", {                      // passport related
+        messages: res.locals.getMessages()   //passport related
+    });
 })
-app.post('/login', function(req, res){
+
+app.post('/login', function(req, res){       //passport related
+  passport.authenticate('local', {           //passport related
+    successRedirect: '/',                    // passport related
+    failureRedirect: '/login',              //passport related
+    failureFlash: true                      //passport related
+  })
   return dal.getUserByUsername(req.body.username).then(function(loginUser){
   console.log('loginUser, loginUser.password, req.body.psw', loginUser, loginUser.password, req.body.psw)
   if(loginUser.password == req.body.psw){
@@ -64,11 +150,7 @@ app.post('/register', function(req, res){
 // -------------All Avatars--------------
 app.get('/', (req,res) =>{
   return dal.getAllCharacters().then(function(characters){
-<<<<<<< HEAD
     res.render('./allAvatars', {characters})
-=======
-    res.render('allAvatars', {characters})
->>>>>>> f03576870558d8f8180b98f3054f3272a0da348c
     console.log('characters', characters)
   })
 })
